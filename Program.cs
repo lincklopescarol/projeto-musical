8dad6852ba7a66db77d9be1253a80ce1c36b9329
lincklopescarol <lincklopescarol@gmail.com>
2023-11-20 23:10:51 -0300

    adicionando condições

--- Program.cs
+++ Program.cs
@@ -14,8 +14,16 @@ void ExibirOpcoesMenu()
     Console.WriteLine("Digite 2 para mostrar todas as bandas.");
     Console.WriteLine("Digite 3 para avaliar uma banda.");
     Console.WriteLine("Digite 4 para exibir a média de uma banda.");
-    Console.WriteLine("Digite 1 para registrar uma banda.");
     Console.WriteLine("Digite -1 para sair.");
+
+    Console.Write("\nDigite sua opção: ");
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+
+    if (opcaoEscolhidaNumerica == 1)
+    {
+        Console.WriteLine("Você digitou a opção " + opcaoEscolhida);
+    }
 }
 
 ExibirMensagemBoasVindas();
