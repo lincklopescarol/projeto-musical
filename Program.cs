fd84441ca57a096e8cd49f31486276bf89eb5a15
lincklopescarol <lincklopescarol@gmail.com>
2023-11-20 23:01:01 -0300

    adicionando função de ExibirOpcoesMenu

--- Program.cs
+++ Program.cs
@@ -8,4 +8,15 @@ void ExibirMensagemBoasVindas()
     Console.WriteLine("***********************************");
 }
 
+void ExibirOpcoesMenu()
+{
+    Console.WriteLine("\nDigite 1 para registrar uma banda.");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas.");
+    Console.WriteLine("Digite 3 para avaliar uma banda.");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda.");
+    Console.WriteLine("Digite 1 para registrar uma banda.");
+    Console.WriteLine("Digite -1 para sair.");
+}
+
 ExibirMensagemBoasVindas();
+ExibirOpcoesMenu();
